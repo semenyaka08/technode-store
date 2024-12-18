7443a380fb3bde72320e975da89e33734006e4ae
semenyaka08 <Serh.semka@gmail.com>
2024-12-18 19:47:12 +0200

    Update logic for category interactions

--- TechNode.Core/Entities/Category.cs
+++ TechNode.Core/Entities/Category.cs
@@ -7,4 +7,13 @@ public class Category : BaseEntity
     public ICollection<Specification> Specifications { get; set; } = [];
     
     public ICollection<Product> Products { get; set; } = [];
+    
+    // Self-referencing relationship
+    public int? ParentCategoryId { get; set; }
+    
+    public Category? ParentCategory { get; set; }
+    
+    public ICollection<Category> ChildCategories { get; set; } = new List<Category>();
+
+    public required bool IsMainCategory { get; set; } = false;
 }
---
+++ TechNode.Infrastructure/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechNode.Core.Entities;
+
+namespace TechNode.Infrastructure.Configuration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasOne(c => c.ParentCategory)
+            .WithMany(c => c.ChildCategories)
+            .HasForeignKey(c => c.ParentCategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.ToTable(table =>
+        {
+            table.HasCheckConstraint("CK_Category_MainCategory",
+                "([IsMainCategory] = 1 AND [ParentCategoryId] IS NULL) OR ([IsMainCategory] = 0)");
+        });
+    }
+}
